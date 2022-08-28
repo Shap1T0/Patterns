163fa58e3b28b17d5cb3f3a37e762b17844606a6
Egor.Nehaenko <nehaenkoegor@gmail.com>
2022-08-28 20:19:59 +0300

    Реализация патерна стратегия

---
+++ Patterns/Patterns/Strategy/Weapon/Bow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Strategy.Weapon
+{
+    public class Bow: IWeaponType
+    {
+        public void MakeDamage()
+        {
+            Console.WriteLine("Выстрел из лука");
+        }
+    }
+}
